a7fba8deae1e4d9a783794b30406dd0e1dcb4dc0
Malena de Juan <malena.dj4@gmail.com>
2021-02-19 10:07:03 +0100

    opción Mario

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-let nombre = "malena";
+const nombre = "malena";
 
 let letraMayus = nombre.charAt(0).toUpperCase();
 
+
+/*Opción Malena*/
 console.log(nombre.replace(nombre[0], letraMayus));
+
+
+/*Opcion 1*/
+console.log(`${nombre.charAt(0).toUpperCase()}${nombre.slice(1)}`);
